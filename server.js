6fb8a62fbf56604302045e9aebfee4c492c423d6
Donavan Genas <donavangenas@gmail.com>
2022-06-02 14:23:54 -0400

    New pokemon created

--- server.js
+++ server.js
@@ -3,14 +3,15 @@ const express = require('express');
 
 const pokemon = require('./models/pokemon');
 
-// Create instance of express
+// Create instance of express/ SETUP
 const app = express();
 const PORT = 3000;
-
-
-//Setup view engine
 app.set('view engine', 'ejs')
 app.set('views', './views')
+
+//* ======== Middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 // Create home route
 app.get('/', (req, res) => {
@@ -21,6 +22,19 @@ app.get('/', (req, res) => {
 app.get('/pokemon', (req, res) => {
     res.render('Index', {data: pokemon})
 })
+
+app.get('/pokemon/new', (req, res) => {
+    res.render('new', {
+        pageTitle: 'New Pokemon',
+        pageHeader: 'Create a new Pokemon'
+    })
+})
+
+app.post('/pokemon', (req, res) => {
+    console.log(req.body)
+    
+})
+
 
 // app.get*"pokemon/:id", (req, res) =>{
 //     res.render("show", {pokemon: pokemon[req.params.id]})
