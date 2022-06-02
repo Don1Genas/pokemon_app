0cc9a2323dd7c7deba6ba9206fa494b7f1705047
Donavan Genas <donavangenas@gmail.com>
2022-06-02 09:35:14 -0400

    Created show views of each pokemon

--- server.js
+++ server.js
@@ -25,12 +25,12 @@ app.get('/pokemon', (req, res) => {
 // app.get*"pokemon/:id", (req, res) =>{
 //     res.render("show", {pokemon: pokemon[req.params.id]})
 
-// app.get('/pokemon/:id', (req, res) => {
-//     console.log(req.params);
-//     res.send(req.params.id)
+app.get('/pokemon/:id', (req, res) => {
+    // console.log(req.params);
+    // // res.send(req.params.id)
     // const result = pokemon.filter(item => item.id === Number(req.params.id))
-    // res.render('Show', {data: result[0]} );
-// })
+    res.render('Show', {data: pokemon, index: req.params.id} );
+})
 
 
 // App Listener
